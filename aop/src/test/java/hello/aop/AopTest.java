82fd73123f954edd42145825d17f4f99881d6c54
iamminseongKim <minseongkimim@gmail.com>
2024-08-16 08:17:03 +0900

    스프링 핵심 원리 - 고급편 - 섹션 10 스프링 AOP 구현 - 완

--- aop/src/test/java/hello/aop/AopTest.java
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -2,10 +2,7 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
-import hello.aop.order.aop.AspectV1;
-import hello.aop.order.aop.AspectV2;
-import hello.aop.order.aop.AspectV3;
-import hello.aop.order.aop.AspectV4Pointcut;
+import hello.aop.order.aop.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
@@ -20,7 +17,9 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 //@Import(AspectV1.class)
 //@Import(AspectV2.class)
 //@Import(AspectV3.class)
-@Import(AspectV4Pointcut.class)
+//@Import(AspectV4Pointcut.class)
+//@Import({AspectV5Order.TxAspect.class, AspectV5Order.LogAspect.class})
+@Import(AspectV6Advice.class)
 public class AopTest {
 
     @Autowired
